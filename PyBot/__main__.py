d9b7b90784a07fbfaff3a49466997386dd930df4
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-01-07 23:43:49 +0530

    changed the file for bot

--- PyBot/__main__.py
+++ PyBot/__main__.py
@@ -1,1 +1,55 @@
-import issue_greeter
+import asyncio
+import importlib
+import os
+import sys
+import traceback
+
+import aiohttp
+from aiohttp import web
+import cachetools
+from gidgethub import aiohttp as gh_aiohttp
+from gidgethub import routing
+from gidgethub import sansio
+
+from . import issue_greeter
+
+import sentry_sdk
+
+router = routing.Router(issue_greeter.router)
+cache = cachetools.LRUCache(maxsize=500)
+
+sentry_sdk.init(os.environ.get("SENTRY_DSN"))
+
+async def main(request):
+    try:
+        body = await request.read()
+        secret = os.environ.get("GH_SECRET")
+        event = sansio.Event.from_http(request.headers, body, secret=secret)
+        print('GH delivery ID', event.delivery_id, file=sys.stderr)
+        if event.event == "ping":
+            return web.Response(status=200)
+        oauth_token = os.environ.get("GH_AUTH")
+        async with aiohttp.ClientSession() as session:
+            gh = gh_aiohttp.GitHubAPI(session, "python/bedevere",
+                                      oauth_token=oauth_token,
+                                      cache=cache)
+          
+            await asyncio.sleep(1)
+            await router.dispatch(event, gh, session=session)
+        try:
+            print('GH requests remaining:', gh.rate_limit.remaining)
+        except AttributeError:
+            pass
+        return web.Response(status=200)
+    except Exception as exc:
+        traceback.print_exc(file=sys.stderr)
+        return web.Response(status=500)
+
+
+if __name__ == "__main__": 
+    app = web.Application()
+    app.router.add_post("/", main)
+    port = os.environ.get("PORT")
+    if port is not None:
+        port = int(port)
+    web.run_app(app, port=port)
--- PyBot/issue_greeter.py
+++ PyBot/issue_greeter.py
@@ -1,7 +1,7 @@
-import os
-import aiohttp
+# import os
+# import aiohttp
 
-from aiohttp import web
+# from aiohttp import web
 
 from gidgethub import routing, sansio
 from gidgethub import aiohttp as gh_aiohttp
@@ -28,26 +28,26 @@ async def issue_opened_event(event, gh, *args, **kwargs):
 
 
 # @routes.post("/")
-async def main(request):
-    body = await request.read()
+# async def main(request):
+#     body = await request.read()
 
-    secret = os.environ.get("GH_SECRET")
-    oauth_token = os.environ.get("GH_AUTH")
+#     secret = os.environ.get("GH_SECRET")
+#     oauth_token = os.environ.get("GH_AUTH")
 
-    event = sansio.Event.from_http(request.headers, body, secret=secret)
-    async with aiohttp.ClientSession() as session:
-        gh = gh_aiohttp.GitHubAPI(session, "vasu-1",
-                                  oauth_token=oauth_token)
-        await router.dispatch(event, gh)
-    return web.Response(status=200)
+#     event = sansio.Event.from_http(request.headers, body, secret=secret)
+#     async with aiohttp.ClientSession() as session:
+#         gh = gh_aiohttp.GitHubAPI(session, "vasu-1",
+#                                   oauth_token=oauth_token)
+#         await router.dispatch(event, gh)
+#     return web.Response(status=200)
 
 
 
-if __name__ == "__main__":
-    app = web.Application()
-    app.router.add_post("/", main)
-    port = os.environ.get("PORT")
-    if port is not None:
-        port = int(port)
+# if __name__ == "__main__":
+#     app = web.Application()
+#     app.router.add_post("/", main)
+#     port = os.environ.get("PORT")
+#     if port is not None:
+#         port = int(port)
 
-    web.run_app(app, port=port)
+#     web.run_app(app, port=port)
