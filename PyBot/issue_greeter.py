0e5b572093b9a6ce11eea4c520c7fb7a7a4e7033
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-01-08 00:01:26 +0530

    commnets removed from issuegreeter

--- PyBot/issue_greeter.py
+++ PyBot/issue_greeter.py
@@ -1,8 +1,3 @@
-# import os
-# import aiohttp
-
-# from aiohttp import web
-
 from gidgethub import routing, sansio
 from gidgethub import aiohttp as gh_aiohttp
 
@@ -13,9 +8,7 @@ router = routing.Router()
 
 @router.register("issues", action="opened")
 async def issue_opened_event(event, gh, *args, **kwargs):
-    """
-    Whenever an issue is opened, greet the author and say thanks.
-    """
+
     url = event.data['issue']['comments_url']
     author = event.data['issue']['user']['login']
     avatar = event.data['issue']['user']['avatar_url']
@@ -24,30 +17,3 @@ async def issue_opened_event(event, gh, *args, **kwargs):
     await gh.post(url, data={
         'body': message,
         })
-
-
-
-# @routes.post("/")
-# async def main(request):
-#     body = await request.read()
-
-#     secret = os.environ.get("GH_SECRET")
-#     oauth_token = os.environ.get("GH_AUTH")
-
-#     event = sansio.Event.from_http(request.headers, body, secret=secret)
-#     async with aiohttp.ClientSession() as session:
-#         gh = gh_aiohttp.GitHubAPI(session, "vasu-1",
-#                                   oauth_token=oauth_token)
-#         await router.dispatch(event, gh)
-#     return web.Response(status=200)
-
-
-
-# if __name__ == "__main__":
-#     app = web.Application()
-#     app.router.add_post("/", main)
-#     port = os.environ.get("PORT")
-#     if port is not None:
-#         port = int(port)
-
-#     web.run_app(app, port=port)
