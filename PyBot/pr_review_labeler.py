60fabcb2e70aa15078470242786cce95f214f547
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-01-08 00:40:13 +0530

    editing required for pr review labeler and greeter

--- PyBot/pr_review_labeler.py
+++ PyBot/pr_review_labeler.py
@@ -8,5 +8,20 @@ router = routing.Router()
 @router.register("pull_request", action="opened")
 async def opened_pr(event, gh, *arg, **kwargs):
     """Mark new PRs as needing a review."""
-    pull_request = event.data["pull_request"]
-    await gh.post(pull_request["labels_url"], data=["review needed"])
+    pull_reques = event.data["pull_request"]["labels"]
+    ur = event.data['pull_request']['comments_url']
+
+
+    messag = f"<table><tbody><tr><td>Thanks for opening the pull request @{author}! I will look into it ASAP!\n Till then you can improve your code & you can show your love by staring my repos 😋.</td></tr></tbody></table>"
+    await gh.post(ur, data={
+        'body': messag,
+        })
+    await gh.post(pull_reques, data=["review_needed"])
+
+
+
+
+    # f'{event.data["pull_request"]["issue_url"]}/labels',
+    #         data=["invalid"]
+
+            
