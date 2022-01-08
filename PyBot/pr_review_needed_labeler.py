023308ee2fed84fa9849c4adf811ef9b49b6901c
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-01-08 19:42:00 +0530

    labels edited

--- PyBot/pr_review_needed_labeler.py
+++ PyBot/pr_review_needed_labeler.py
@@ -11,6 +11,6 @@ LABEL1 = 'review_needed_pr' # label name
 
 @router.register("pull_request", action="opened")
 async def issue_opened_event(event, gh, *args, **kwargs):
-    label = event.data['pull_request']['labels_url']
+    label = event.data['pull_request']['labels']
 
     await gh.post(label, data=[LABEL1]) #event post for key label
